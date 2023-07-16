5064c71e7d7ebbba7ef479a731b488f5c08ff0c5
Aliaksei <sladkiy.aleksey@gmail.com>
2023-07-16 13:33:11 +0200

    feat: entry point

--- asyncRace/src/main.ts
+++ asyncRace/src/main.ts
@@ -1,1 +1,11 @@
+import EventHandler from './modules/eventHandler';
+import EventListener from './modules/eventListener';
+import storageService from './modules/storageService';
+import templateBuilder from './modules/templateBuilder';
 import './style.scss';
+
+const storage = await storageService.initialization();
+const template = templateBuilder.initialization(storage);
+const helper = new EventHandler(template, storage);
+const listener = new EventListener(template, storage, helper);
+listener.listen();
